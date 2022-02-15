2433663fa8b0d7ad67f4f75fb82b9a5bfb9194e2
ai.reskays <ai.reskays@tensor.ru>
2022-02-15 11:01:27 +0300

    stage2/ try 1

--- backend/foodgram_project/foodgram_project/settings.py
+++ backend/foodgram_project/foodgram_project/settings.py
@@ -109,7 +109,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/mediafiles/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles/')
 
 
 AUTH_USER_MODEL = 'users.FoodgramUser'
